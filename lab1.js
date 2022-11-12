e5a02d496b41eb4adb40df21e2815a9335932f89
felicia <feliciawillnas@gmail.com>
2022-11-12 21:55:12 +0100

    first commit

---
+++ lab1.js
@@ -0,0 +1,30 @@
+const inventory = []; 
+
+function addToInventory() {
+    const addItem = prompt ("ADD ITEM"); 
+    inventory.push(addItem); 
+    console.log(inventory); 
+}
+
+//while(true) {
+//    let input = prompt("add item"); 
+//    inventory.push(input); 
+//    console.log(inventory); 
+//}
+
+
+
+// function addToInventory() {
+//    const addItem = prompt("add item:"); 
+
+//    for (const item of inventory) {
+//        console.log(item);
+//    }
+//}
+
+
+
+//for (const item of inventory) {
+//    console.log (inventory);
+//}
+ 
