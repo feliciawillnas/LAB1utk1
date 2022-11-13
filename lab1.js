6edb45858ab051aa4d7b7e181c12b0f95af18a63
felicia <feliciawillnas@gmail.com>
2022-11-13 02:23:07 +0100

    functions

--- lab1.js
+++ lab1.js
@@ -1,15 +1,44 @@
 const inventory = []; 
+const inkoplista = [apple, bread, milk]; 
 
 function addToInventory() {
     const addItem = prompt ("ADD ITEM"); 
     inventory.push(addItem); 
     console.log(inventory); 
+
+    ///////////////
+      if (addItem === "apple")
+      alert("rätt"); 
+    /////// idk om det ska vara såhär 
 }
+
 
 ///////////////////
 
 function showItemList() {
-    var x = document.getElementById("itemlist");
+    var displayItemList = document.getElementById("itemlist");
+
+    if (displayItemList.style.display === "block") {
+      displayItemList.style.display = "none";
+    } 
+    
+    else {
+      displayItemList.style.display = "block";
+    }
+  }
+ 
+  ////////////////
+
+  function showCart() {
+    var displayCart = (inventory);
+    alert(displayCart);
+  }
+
+  /////////////////
+
+
+  function fruitArea() {
+    var x = document.getElementById("fruitarea");
 
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -19,5 +48,3 @@ function showItemList() {
       x.style.display = "block";
     }
   }
- 
-  ////////////////
