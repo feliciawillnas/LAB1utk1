a51337872e47a334c91c6f3aa2a9baedae2f3380
felicia <feliciawillnas@gmail.com>
2022-11-15 23:04:51 +0100

    .

---
+++ hej.js
@@ -0,0 +1,5 @@
+array1 = ["orange", "pear", "milk"];
+array2 = ["dog", "pear", "orange"]; 
+
+
+const intersection = array1.filter(element => array2.includes(element));
--- lab12.js
+++ lab12.js
@@ -1,7 +1,8 @@
 let inventory= [];
+let desiredItems = ["apple", "bread", "pear"];
 
-const addItem = (ev)=>{
-    ev.preventDefault();  
+const addItem = (x)=>{
+    x.preventDefault();  
     let addedItem = document.getElementById('addtoinventory').value
 
     inventory.push(addedItem);
@@ -12,23 +13,21 @@ const addItem = (ev)=>{
 
 document.addEventListener('DOMContentLoaded', ()=>{
 document.getElementById('formbutton').addEventListener('click', addItem);
-
-console.log (inventory); 
-
 });
-
 
 ///////////////////
 
 function showItemList() {
-    var displayItemList = document.getElementById("itemlist");
+    var k = (desiredItems); 
+    x = document.getElementById("itemlist");
 
-    if (displayItemList.style.display === "block") {
-      displayItemList.style.display = "none";
+    document.getElementById("itemlist").innerHTML = "your cart: " + k; 
+
+    if (x.style.display === "block") {
+      x.style.display = "none";
     } 
-    
     else {
-      displayItemList.style.display = "block";
+      x.style.display = "block";
     }
   }
  
@@ -52,7 +51,34 @@ function showItemList() {
 
   function fruitArea() {
     var x = document.getElementById("fruitarea"); 
-    var k = document.getElementById("entrance"); 
+
+    if (x.style.display === "block") {
+      x.style.display = "none"; 
+    }
+    else {
+      x.style.display = "block"; 
+    }
+  }
+
+  function breadSection() {
+    var x = document.getElementById("bread"); 
+
+    if (x.style.display === "block") {
+      x.style.display = "none"; 
+    }
+    else {
+      x.style.display = "block"; 
+    }
+  }
+
+  function makePayment() {
+
+    var cart = (inventory); 
+    x = document.getElementById("payment");
+
+    document.getElementById("payment").innerHTML = "here is your cart:" + cart + "would you like to proceed?"; 
+
+    var x = document.getElementById("payment"); 
 
     if (x.style.display === "block") {
       x.style.display = "none"; 
